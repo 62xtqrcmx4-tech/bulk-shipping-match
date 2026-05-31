d8febb16cf40ecf08c086d2d62223547bc7f0be9
62xtqrcmx4-tech <356190646@qq.com>
2026-05-31 08:00:10 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import SiteHeader from "../components/SiteHeader";
 
 export const metadata: Metadata = {
   title: "DMU船货匹配平台",
-  description: "辽宁省智慧交通与港航物流工程研究中心",
+  description: "辽宁省智慧交通与港航物流工程研究中心 V0.1",
 };
 
 export default function RootLayout({
