d0a506b6ef20cdfd055d66f5c28e001ccc620292
62xtqrcmx4-tech <356190646@qq.com>
2026-06-01 15:22:58 +0800

    Create AppFooter.tsx

---
+++ components/AppFooter.tsx
@@ -0,0 +1,9 @@
+export default function AppFooter() {
+  return (
+    <div className="mx-auto max-w-7xl px-6 pb-8">
+      <div className="pt-4 text-sm font-medium text-blue-700">
+        辽宁省智慧交通与港航物流工程研究中心开发 V0.1
+      </div>
+    </div>
+  );
+}
